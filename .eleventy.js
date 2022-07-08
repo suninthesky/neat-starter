cf68e3fcc737064241c19280844393e2599ea60d
Suraj Vadgama <suninthesky@users.noreply.github.com>
2022-07-08 08:52:27 +0100

    Sticky footer

--- .eleventy.js
+++ .eleventy.js
@@ -28,8 +28,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
   });
 
   // Copy Image Folder to /_site
